f483972db6cc6d1f865f78097369c20366e69cef
Michael Smith <msmithboise@gmail.com>
2018-12-18 23:13:22 -0700

    adding vue router

--- wwwroot/wwwsrc/client/src/main.js
+++ wwwroot/wwwsrc/client/src/main.js
@@ -3,9 +3,11 @@ import App from './App.vue'
 import store from './store'
 import './registerServiceWorker'
 
+
 Vue.config.productionTip = false
 
 new Vue({
   store,
+
   render: h => h(App)
 }).$mount('#app')

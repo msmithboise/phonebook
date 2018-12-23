1e0b60a5c0ce16c1726a41cf271f368bf733d5be
Michael Smith <msmithboise@gmail.com>
2018-12-22 20:08:15 -0700

    repository and interface added

---
+++ Interfaces/IRepositoryBase.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Text;
+
+namespace Contacts
+{
+    public interface IRepositoryBase<T>
+    {
+        IEnumerable<T> FindAll();
+        IEnumerable<T> FindByCondition(Expression<Func<T, bool>> expression);
+        void Create(T entity);
+        void Update(T entity);
+        void Delete(T entity);
+        void Save();
+    }
+}
---
+++ Models/Repository/RepositoryBase.cs
@@ -0,0 +1,52 @@
+using Contacts;
+using Phonebook;
+using Phonebook.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+
+
+
+namespace Repository
+{
+    public abstract class RepositoryBase<T> : IRepositoryBase<T> where T : class
+    {
+        protected Phonebook11Context Phonebook11Context { get; set; }
+
+        public RepositoryBase(Phonebook11Context phonebook11Context)
+        {
+            this.Phonebook11Context = phonebook11Context;
+        }
+
+        public IEnumerable<T> FindAll()
+        {
+            return this.Phonebook11Context.Set<T>();
+        }
+
+        public IEnumerable<T> FindByCondition(Expression<Func<T, bool>> expression)
+        {
+            return this.Phonebook11Context.Set<T>().Where(expression);
+        }
+
+        public void Create(T entity)
+        {
+            this.Phonebook11Context.Set<T>().Add(entity);
+        }
+
+        public void Update(T entity)
+        {
+            this.Phonebook11Context.Set<T>().Update(entity);
+        }
+
+        public void Delete(T entity)
+        {
+            this.Phonebook11Context.Set<T>().Remove(entity);
+        }
+
+        public void Save()
+        {
+            this.Phonebook11Context.SaveChanges();
+        }
+    }
+}
